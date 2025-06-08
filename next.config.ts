8e5e27468d539fea98c25404ad0fe44ba2bef88e
Joe <joethai@calibrated.io>
2025-06-08 10:23:37 -0500

    edit script

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import { env } from '@/env'
 import type { NextConfig } from 'next'
 
-const storageUrl = process.env.NEXT_PUBLIC_STORAGE_URL!
-
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [storageUrl]?.map((item) => {
+    remotePatterns: [env.NEXT_PUBLIC_STORAGE_URL]?.map((item) => {
       const url = new URL(item)
       return {
         protocol: 'https',
