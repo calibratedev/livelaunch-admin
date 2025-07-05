7b7712f995ff1f3d22bf306d5b5f6159f1853116
Joe <joethai@calibrated.io>
2025-07-05 16:05:14 +0700

    usesearch param

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,19 +1,7 @@
 'use client'
 
 import ProfileTab from '@/components/settings/profile-tab'
-import { useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
-import { toast } from 'sonner'
 
 export default function SettingsPage() {
-  const searchParams = useSearchParams()
-
-  useEffect(() => {
-    const error = searchParams.get('error')
-    if (!!error) {
-      toast.error(error)
-    }
-  }, [])
-
   return <ProfileTab />
 }
