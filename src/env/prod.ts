c71a14191df580636c391451ec25fd991017008d
Joe <joethai@calibrated.io>
2026-01-05 20:57:02 +0700

    fix env

--- src/env/prod.ts
+++ src/env/prod.ts
@@ -1,5 +1,5 @@
 export const env = {
-  BUILD_ENV: 'dev',
+  BUILD_ENV: 'prod',
   NEXT_PUBLIC_API_URL: 'https://api.livelaunch.io',
   NEXT_PUBLIC_STORAGE_URL: 'https://storage.livelaunch.io',
   NEXT_PUBLIC_SHARE_URL: 'https://scan.livelaunch.io',
